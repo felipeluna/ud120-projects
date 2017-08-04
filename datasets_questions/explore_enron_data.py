15421f116a7d06774ac573421dab22c77906b26d
Felipe Luna <fsl@cesar.org.br>
2017-08-04 07:39:16 -0300

    finishing lesson 6

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,7 +16,26 @@
 """
 
 import pickle
+import pprint
 
+pp = pprint.PrettyPrinter(depth=6)
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-
+# print enron_data
+# pois = open("../final_project/poi_names.txt").read().split('\n')[2:-1]
+# print pois
+poi = {k: v for k, v in enron_data.iteritems() if v['poi'] == True }
+salary = {k: v for k, v in enron_data.iteritems() if v['salary'] != 'NaN' }
+salary = {k: v for k, v in enron_data.iteritems() if v['salary'] != 'NaN' }
+email = {k: v for k, v in enron_data.iteritems() if v['email_address'] != 'NaN' }
+missing_payments = {k: v for k, v in enron_data.iteritems() if v['total_payments'] == 'NaN' }
+have = {k: v for k, v in enron_data.iteritems() if v['total_payments'] != 'NaN' }
+poi_missing_payments = {k: v for k, v in enron_data.iteritems() if v['total_payments'] == 'NaN'  and v['poi'] == True}
+print len(salary.keys())
+print len(email.keys()) 
+print len(missing_payments.keys())
+print len(have.keys())
+print len(poi_missing_payments.keys())
+print len(enron_data)
+print len(poi.keys())
+# pp.pprint(missing_payments)
